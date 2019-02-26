ec67d163f7d9734f9e40969b6b84f742c5ffa774
mif <mif.open@gmail.com>
2019-02-26 07:24:33 +0500

    System.CommandLine.Experimental -> CommandLineParser

--- src/LazyCoder.Runner/Program.cs
+++ src/LazyCoder.Runner/Program.cs
@@ -1,25 +1,22 @@
-﻿using System.CommandLine;
-using System.CommandLine.Builder;
-using System.CommandLine.Invocation;
-using System.Threading.Tasks;
+﻿using CommandLine;
 
 namespace LazyCoder.Runner
 {
     class Program
     {
-        private static async Task<int> Main(string[] args)
+        private static void Main(string[] args)
         {
-            var parser = new CommandLineBuilder(new Command("lazy", handler: CommandHandler.Create(typeof(Runner).GetMethod(nameof(Runner.Run)))))
-                         .AddOption(new Option(new[] { "-dll" },
-                                               argument: new Argument<string>("../../tests/LazyCoder.TestDll/bin/Debug/netstandard2.0/LazyCoder.TestDll.dll")
-                                                         { Arity = ArgumentArity.ExactlyOne }))
-                         .AddOption(new Option(new[] { "-o", "-output", "-outputDirectory" },
-                                               argument: new Argument<string>("./output")
-                                                         { Arity = ArgumentArity.ExactlyOne }))
-                         .UseExceptionHandler()
-                         .Build();
+            Parser.Default.ParseArguments<Options>(args)
+                  .WithParsed(o => Runner.Run(o.Dll, o.Output));
+        }
 
-            return await parser.InvokeAsync(args).ConfigureAwait(false);
+        private class Options
+        {
+            [Option("dll", Default = "../../tests/LazyCoder.TestDll/bin/Debug/netstandard2.0/LazyCoder.TestDll.dll")]
+            public string Dll { get; set; }
+
+            [Option("output", Default = "./output")]
+            public string Output { get; set; }
         }
     }
 }
