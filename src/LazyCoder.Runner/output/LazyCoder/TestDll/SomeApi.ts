d570ca5bc5201bd836b48ade6a239b731b3eeea4
mif <mif.open@gmail.com>
2019-03-03 09:08:44 +0500

    clean up

--- src/LazyCoder.Runner/output/LazyCoder/TestDll/SomeApi.ts
+++
@@ -1,9 +0,0 @@
-export namespace SomeApi {
-    export function SomeAction(value: string): number {
-        // some body
-    }
-
-    export function SomeOtherAction(value: number): boolean {
-        // some body
-    }
-}
--- src/LazyCoder.Runner/output/LazyCoder/TestDll/SomeEnum.ts
+++
@@ -1,5 +0,0 @@
-export enum SomeEnum {
-    FirstValue = 0,
-    SecondValue = 1,
-    ThirdValue = 2,
-}
