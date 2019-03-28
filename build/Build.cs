2178842f14e999d3ce2d6e76c4025bab2047dbb7
mif <mif.open@gmail.com>
2019-03-28 21:30:04 +0500

    script for nuget publishing

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.Execution;
@@ -8,7 +7,6 @@ using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -19,7 +17,11 @@ class Build: NukeBuild
     public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    readonly Configuration Configuration =
+        IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter("NuGet api key")] readonly string ApiKey;
+    readonly string LicenseFile = RootDirectory / "LICENSE";
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
@@ -29,34 +31,92 @@ class Build: NukeBuild
     AbsolutePath TestsDirectory => RootDirectory / "tests";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
-    Target Clean => _ => _
-                         .Before(Restore)
-                         .Executes(() =>
-                                   {
-                                       SourceDirectory
-                                           .GlobDirectories("**/bin", "**/obj").ToArray().ForEach(DeleteDirectory);
-                                       TestsDirectory
-                                           .GlobDirectories("**/bin", "**/obj").ToArray().ForEach(DeleteDirectory);
-                                       EnsureCleanDirectory(ArtifactsDirectory);
-                                   });
+    Target Clean
+        => _ => _
+                .Before(Restore)
+                .Executes(() =>
+                          {
+                              SourceDirectory.GlobDirectories("**/bin", "**/obj")
+                                             .ToArray()
+                                             .ForEach(DeleteDirectory);
+                              TestsDirectory.GlobDirectories("**/bin", "**/obj")
+                                            .ToArray()
+                                            .ForEach(DeleteDirectory);
+                              EnsureCleanDirectory(ArtifactsDirectory);
+                          });
 
-    Target Restore => _ => _
-                          .Executes(() =>
-                                    {
-                                        DotNetRestore(s => s
-                                                          .SetProjectFile(Solution));
-                                    });
+    Target Restore
+        => _ => _
+               .Executes(() =>
+                         {
+                             DotNetRestore(s => s
+                                               .SetProjectFile(Solution));
+                         });
 
-    Target Compile => _ => _
-                           .DependsOn(Restore)
-                           .Executes(() =>
-                                     {
-                                         DotNetBuild(s => s
-                                                          .SetProjectFile(Solution)
-                                                          .SetConfiguration(Configuration)
-                                                          .SetAssemblyVersion(GitVersion.GetNormalizedAssemblyVersion())
-                                                          .SetFileVersion(GitVersion.GetNormalizedFileVersion())
-                                                          .SetInformationalVersion(GitVersion.InformationalVersion)
-                                                          .EnableNoRestore());
-                                     });
+    Target Compile
+        => _ => _
+                .DependsOn(Restore)
+                .Executes(() =>
+                          {
+                              DotNetBuild(s => s
+                                               .SetProjectFile(Solution)
+                                               .SetConfiguration(Configuration)
+                                               .SetAssemblyVersion(GitVersion
+                                                                       .GetNormalizedAssemblyVersion())
+                                               .SetFileVersion(GitVersion
+                                                                   .GetNormalizedFileVersion())
+                                               .SetInformationalVersion(GitVersion
+                                                                            .InformationalVersion)
+                                               .EnableNoRestore());
+                          });
+
+    Target Pack
+        => _ => _
+                .DependsOn(Clean, Compile)
+                .Executes(() =>
+                          {
+                              var licenseUrl =
+                                  GitRepository.GetGitHubBrowseUrl(LicenseFile, "master");
+                              DotNetPack(s => s
+                                              //.SetPackageReleaseNotes(changelogUrl)
+                                              .SetWorkingDirectory(RootDirectory)
+                                              .SetPackageLicenseUrl(licenseUrl)
+                                              .SetProject(Solution.Path)
+                                              .EnableNoBuild()
+                                              .SetConfiguration(Configuration)
+                                              .EnableIncludeSymbols()
+                                              .SetOutputDirectory(ArtifactsDirectory)
+                                              .SetDescription("Writes Typescript for you")
+                                              .SetAuthors("mif")
+                                              .SetVersion(GitVersion.NuGetVersionV2));
+                          });
+
+    Target Publish
+        => _ => _
+                .DependsOn(Pack)
+                .Requires(() => ApiKey)
+                .Requires(() => Equals(Configuration, Configuration.Release))
+                .Executes(() =>
+                          {
+                              GlobFiles(ArtifactsDirectory, "*.nupkg")
+                                  .NotEmpty()
+                                  .Where(x => !x.EndsWith(".symbols.nupkg"))
+                                  .ForEach(x => DotNetNuGetPush(s => s
+                                                                     .SetTargetPath(x)
+                                                                     .SetSource("https://api.nuget.org/v3/index.json")
+                                                                     .SetApiKey(ApiKey)));
+                          });
+
+    Target PublishLocal
+        => _ => _
+                .DependsOn(Pack)
+                .Executes(() =>
+                          {
+                              GlobFiles(ArtifactsDirectory, "*.nupkg")
+                                  .NotEmpty()
+                                  .Where(x => !x.EndsWith(".symbols.nupkg"))
+                                  .ForEach(x => DotNetNuGetPush(s => s
+                                                                     .SetTargetPath(x)
+                                                                     .SetSource("/Users/mif/Documents/GitHub/localnuget")));
+                          });
 }
